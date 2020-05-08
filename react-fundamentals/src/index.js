c2f42c045917657b3dc7469587fd4b2b3e3ffb2e
Julian <julianmahecha@outlook.com>
2020-05-08 15:01:22 -0500

    Using Classes

--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -17,21 +17,87 @@ const App = <h1> {getInfo(user1)} </h1>
 ReactDOM.render(App, document.getElementById('root')); */
 
 
-
-const TarjetaFruta = () => (
+/* 
+const TarjetaFruta = (props) => (
   <div>
-    <h3>
+    <h3>{props.name}</h3>
       <hr/>
-      <p>Description .... changue</p>
-    </h3>
+      <p>{props.price}</p>
   </div>
 )
 
 
 const App = () => (
   <div>
-    <TarjetaFruta />
+    <TarjetaFruta name = 'Sandia' price = {2.00} />
+    <TarjetaFruta name = 'Maranja' price = {4.00} />
+    <TarjetaFruta name = 'Kiwi' price = {6.00} />
   </div>
 )
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root')); */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class TarjetaFruta extends React.Component {
+  constructor () {
+    super()
+
+    const METHODS = [
+      'agregar',
+      'quitar',
+      'limpiar'
+    ]
+
+    METHODS.forEach((method) => {
+      this[method] = this[method].bind(this)
+    })
+
+    this.state = {
+      cantidad: 0
+    }
+  }
+
+  agregar () {
+    this.setState({
+      cantidad: this.state.cantidad + 1
+    })
+  }
+
+  quitar () {
+    this.setState({
+      cantidad: this.state.cantidad - 1
+    })
+  }
+
+  limpiar () {
+    this.setState({
+      cantidad: 0
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <h3>{ this.props.name }</h3>
+        <div>Cantidad: { this.state.cantidad }</div>
+        <button onClick={this.agregar} > + </button>
+        <button onClick={this.quitar} > - </button>
+        <button onClick={this.limpiar} > Limpiar</button>        
+        <hr/>
+        <p>$ { this.props.price }</p>
+      </div>
+    )
+  }
+}
+
+const App = () => (
+  <div>
+    <TarjetaFruta name={'Sandia'} price={5.00} />
+    <TarjetaFruta name={'Naranja'} price={1.50} />
+    <TarjetaFruta name='Kiwi' price={3.30} />
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
